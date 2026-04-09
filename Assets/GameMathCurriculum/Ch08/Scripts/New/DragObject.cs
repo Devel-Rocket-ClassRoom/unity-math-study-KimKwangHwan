7197dd5a7eae83b32765b8d1bb7c6fe34f2c0df8
KimKwangHwan <louie1004@naver.com>
2026-04-10 01:19:36 +0900

    드래그앤드롭 새로운 버전 수정

--- Assets/GameMathCurriculum/Ch08/Scripts/New/DragObject.cs
+++ Assets/GameMathCurriculum/Ch08/Scripts/New/DragObject.cs
@@ -6,27 +6,42 @@ public class DragObject : MonoBehaviour, IDraggable
     private Vector3 startPosition;
     private bool inDropZone;
     private bool isDragged;
+    private bool isDragging;
     private Vector3 draggedVector;
+    private Transform dropZone;
     [SerializeField] private Terrain terrain;
     [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private Color color;
     private void Awake()
     {
         inDropZone = false;
+        isDragged = false;
+        isDragging = false;
+        GetComponent<Renderer>().material.color = color;
     }
     public void OnDragStart(Vector3 hitPoint)
     {
         startPosition = transform.position;
         draggedVector = transform.position - new Vector3(hitPoint.x, terrain.SampleHeight(transform.position) + GetComponent<Renderer>().bounds.size.y / 2f, hitPoint.z);
+        isDragging = true;
     }
 
     public void OnDrag(Vector3 hitPoint)
     {
-        
+        transform.position = new Vector3(hitPoint.x, terrain.SampleHeight(transform.position) + GetComponent<Renderer>().bounds.size.y / 2f, hitPoint.z) + draggedVector;
     }
 
     public void OnDragEnd()
     {
-        isDragged = true;
+        isDragging = false;
+        if (inDropZone && dropZone != null)
+        {
+            transform.position = new Vector3(dropZone.position.x, dropZone.position.y + dropZone.GetComponent<Renderer>().bounds.size.y / 2f + GetComponent<Renderer>().bounds.size.y / 2f, dropZone.position.z);
+        }
+        else
+        {
+            isDragged = true;
+        }
     }
 
     private void Update()
@@ -43,16 +58,18 @@ public class DragObject : MonoBehaviour, IDraggable
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("DropZone"))
+        if (isDragging && other.CompareTag("DropZone"))
         {
             inDropZone = true;
+            dropZone = other.transform;
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("DropZone"))
+        if (isDragging && other.CompareTag("DropZone"))
         {
             inDropZone = false;
+            dropZone = null;
         }
     }
 
